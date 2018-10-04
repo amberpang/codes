3b7337056620d1a3284f490a82ad6447f4fecb05
sihui pang <sihuipang@gmail.com>
2018-10-04 20:19:22 +1000

    zigzag

---
+++ zigzag.py
@@ -0,0 +1,29 @@
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this:
+(you may want to display this pattern in a fixed font for better legibility)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+string convert(string s, int numRows);
+"""
+
+def convert(s,numRows):
+
+    if numRows == 1 or len(s) < numRows:
+        return s
+
+    index, step = 0, 1
+    res = [''] * numRows
+    for i in s:
+        res[index] += i
+        if index == 0:
+            step = 1
+        elif index == numRows - 1:
+            step = -1
+        index += step
+    return "".join(res)
+
+s = "PAYPALISHIRING"
+print (convert(s, 3))
