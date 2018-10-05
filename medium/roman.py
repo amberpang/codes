91d5f6cb0a263baf22bc65f3b89a0320781a41e6
sihui pang <sihuipang@gmail.com>
2018-10-05 11:25:04 +1000

    roman

--- medium/roman.py
+++ medium/roman.py
@@ -12,5 +12,31 @@ M             1000
 For example, two is written as II in Roman numeral, just two one's added together.
 Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII,
 which is XX + V + II.
+Roman numerals are usually written largest to smallest from left to right.
+However, the numeral for four is not IIII. Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four. The same principle applies to the number nine,
+which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given an integer, convert it to a roman numeral.
+Input is guaranteed to be within the range from 1 to 3999.
 
 """
+
+
+def intToRoman(num):
+    """
+    :type num: int
+    :rtype: str
+    """
+    M = ["", "M", "MM", "MMM"]
+    C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+    X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+    I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+    res = M[int(num / 1000)] + C[int((num % 1000) / 100)] + X[int((num % 100) / 10)] + I[int(num % 10)]
+    return res
+
+num = input()
+print(intToRoman(num))
